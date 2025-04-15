e35157a53249623cf3d7dcb40e41df09441b7090
Santiago Acosta <santi.acosta105@gmail.com>
2025-04-14 21:43:16 -0300

    Backend listo para deploy a Render

--- index.js
+++ index.js
@@ -27,4 +27,4 @@ app.post('/send', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
-}); //cambio git
+});
