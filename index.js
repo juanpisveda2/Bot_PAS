d44b09794221e50e4a52043bd3e9434398bc0ba3
juanpisveda2 <juanpisveda13@gmail.com>
2025-04-15 16:37:17 -0300

    prueba

--- index.js
+++ index.js
@@ -27,8 +27,4 @@ app.post('/send', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
-<<<<<<< HEAD
 }); //cambio git
-=======
-});
->>>>>>> e35157a53249623cf3d7dcb40e41df09441b7090
